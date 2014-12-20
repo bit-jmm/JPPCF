81d01d03cb71184790c9f9bfdaf4a0a4ec8c0bb0
bit-jmm <mingj21@163.com>
2014-12-20 22:26:56 +0800

    add some processes and result change

--- start_filter_by_10.sh
+++ start_filter_by_10.sh
@@ -6,10 +6,18 @@
 #nohup python demo/demo_at_tsinghua_filter_by_10.py 100 100 >> ./log/tsinghua_server/filter_by_10/status_log/k_100_lambda_100.txt 2>&1 &
 #nohup python demo/demo_at_tsinghua_filter_by_10.py 100 500 >> ./log/tsinghua_server/filter_by_10/status_log/k_100_lambda_500.txt 2>&1 &
 
-nohup python demo/demo_at_tsinghua_filter_by_10.py 200 0.5 >> ./log/tsinghua_server/filter_by_10/status_log/k_200_lambda_0.5.txt 2>&1 &
-nohup python demo/demo_at_tsinghua_filter_by_10.py 200 1 >> ./log/tsinghua_server/filter_by_10/status_log/k_200_lambda_1.txt 2>&1 &
-nohup python demo/demo_at_tsinghua_filter_by_10.py 200 5 >> ./log/tsinghua_server/filter_by_10/status_log/k_200_lambda_5.txt 2>&1 &
+#nohup python demo/demo_at_tsinghua_filter_by_10.py 200 0.5 >> ./log/tsinghua_server/filter_by_10/status_log/k_200_lambda_0.5.txt 2>&1 &
+#nohup python demo/demo_at_tsinghua_filter_by_10.py 200 1 >> ./log/tsinghua_server/filter_by_10/status_log/k_200_lambda_1.txt 2>&1 &
+#nohup python demo/demo_at_tsinghua_filter_by_10.py 200 5 >> ./log/tsinghua_server/filter_by_10/status_log/k_200_lambda_5.txt 2>&1 &
+nohup python demo/demo_at_tsinghua_filter_by_10.py 200 8 >> ./log/tsinghua_server/filter_by_10/status_log/k_200_lambda_8.txt 2>&1 &
 #nohup python demo/demo_at_tsinghua_filter_by_10.py 200 10 >> ./log/tsinghua_server/filter_by_10/status_log/k_200_lambda_10.txt 2>&1 &
+nohup python demo/demo_at_tsinghua_filter_by_10.py 200 15 >> ./log/tsinghua_server/filter_by_10/status_log/k_200_lambda_15.txt 2>&1 &
+nohup python demo/demo_at_tsinghua_filter_by_10.py 200 20 >> ./log/tsinghua_server/filter_by_10/status_log/k_200_lambda_20.txt 2>&1 &
+nohup python demo/demo_at_tsinghua_filter_by_10.py 200 25 >> ./log/tsinghua_server/filter_by_10/status_log/k_200_lambda_25.txt 2>&1 &
+nohup python demo/demo_at_tsinghua_filter_by_10.py 200 30 >> ./log/tsinghua_server/filter_by_10/status_log/k_200_lambda_30.txt 2>&1 &
+nohup python demo/demo_at_tsinghua_filter_by_10.py 200 35 >> ./log/tsinghua_server/filter_by_10/status_log/k_200_lambda_35.txt 2>&1 &
+nohup python demo/demo_at_tsinghua_filter_by_10.py 200 40 >> ./log/tsinghua_server/filter_by_10/status_log/k_200_lambda_40.txt 2>&1 &
+nohup python demo/demo_at_tsinghua_filter_by_10.py 200 45 >> ./log/tsinghua_server/filter_by_10/status_log/k_200_lambda_45.txt 2>&1 &
 #nohup python demo/demo_at_tsinghua_filter_by_10.py 200 50 >> ./log/tsinghua_server/filter_by_10/status_log/k_200_lambda_50.txt 2>&1 &
 #nohup python demo/demo_at_tsinghua_filter_by_10.py 200 100 >> ./log/tsinghua_server/filter_by_10/status_log/k_200_lambda_100.txt 2>&1 &
 #nohup python demo/demo_at_tsinghua_filter_by_10.py 200 500 >> ./log/tsinghua_server/filter_by_10/status_log/k_200_lambda_500.txt 2>&1 &
