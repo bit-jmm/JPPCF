c08114422a0a45a8c29ea9b898e5e6db9cd071b9
bit-jmm <mingj21@163.com>
2015-01-15 10:37:11 +0800

    commit jppcf_with_topic result

--- start_jppcf_with_topic.sh
+++ start_jppcf_with_topic.sh
@@ -1,9 +1,9 @@
 #for (( i=15; i < 30; i++))
 for k in 200
 do
-        for i in 1 5 10 15
+        for i in 1 5 10 
         do
-            for eta in 0.2 0.4 0.5 0.7 0.9 1.0
+            for eta in 0.2 0.4 0.5 0.7 0.9
             do
                 nohup python demo/jppcf_with_topic.py $k $i $eta >> ./log/redirect_log_k_"$k"_lambda_"$i"_eta_"$eta".log 2>&1 &
             done
