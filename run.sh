78dee31f780fc06470088aaaddd3e0e243c70158
bit-jmm <mingj21@163.com>
2015-05-05 09:28:17 +0800

    fix bug in timesvdpp and run.sh model name bug

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 for i in 1
 do
-  for model in trm timesvdpp wals tendsorals pmf
+  #for model in trm timesvdpp wals tensorals pmf
+  for model in timesvdpp tensorals
   do
     nohup python demo.py $model $i >> log/stdout_"$model"_k_20_"$i"_MovieLens.log 2>&1 &
   done
