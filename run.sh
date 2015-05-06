bd5866732b48e4d24ff8f8e64d5da01f2b18db45
bit-jmm <mingj21@163.com>
2015-05-06 10:05:29 +0800

    remove trm result on Citeulike2 because of max score bug

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-for i in 4 5
+for i in 1 2 3 4 5
 do
   #for model in trm timesvdpp wals tensorals pmf
-  for model in wals tensorals pmf
+  for model in trm
   #for model in trm timesvdpp
   do
     nohup python demo.py $model $i >> log/stdout_"$model"_k_20_"$i"_CiteUlike2.log 2>&1 &
